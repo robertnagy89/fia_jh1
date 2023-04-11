beba4e4cc9ff5eb34f3b904f957f0bea28fc13f0
Róbert Nagy <roister@windowslive.com>
2023-04-11 23:23:25 +0200

    Feat: Add UserResource and inital FiaDbContext

---
+++ API/FIA/FIA.Api/Models/UserResource.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIA.Api.Models
+{
+    public class UserResource
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+    }
+}
