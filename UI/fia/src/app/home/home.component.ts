efeb5bbe804de68431dd9a099b0a19d9cfbfd1c4
Róbert Nagy <roister@windowslive.com>
2023-04-19 23:06:46 +0200

    Feat: Add form validation to HomeComponent

--- UI/fia/src/app/home/home.component.ts
+++ UI/fia/src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  userForm: FormGroup = new FormGroup({});
+  submitted = false;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.userForm = this.formBuilder.group({
+      name: ["", [Validators.required, Validators.minLength(3), Validators.maxLength(20)]]
+    });
+  }
+
+  submitForm() {
+    this.submitted = true;
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    }
+  }
 }
