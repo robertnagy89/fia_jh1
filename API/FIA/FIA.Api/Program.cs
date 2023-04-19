d69fbec030a14526eb0488b3cd03ebb1cdb3b12b
Róbert Nagy <roister@windowslive.com>
2023-04-19 19:24:06 +0200

    Feat: Add ChatService

--- API/FIA/FIA.Api/Program.cs
+++ API/FIA/FIA.Api/Program.cs
@@ -1,10 +1,15 @@
 using FIA.Api.Data;
+using FIA.Api.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<ChatService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
