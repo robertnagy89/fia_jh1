2a1289c6f9be9cf99251f8ee31e2e27e3be04a5b
Róbert Nagy <roister@windowslive.com>
2023-04-20 11:36:49 +0200

    Feat: Add navigation between component views

--- UI/fia/src/app/chat/chat.component.ts
+++ UI/fia/src/app/chat/chat.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
 
+
+export class ChatComponent {
+  @Output() closeChatEmitter = new EventEmitter();
+
+  backToHome() {
+    this.closeChatEmitter.emit();
+  }
 }
--- UI/fia/src/app/home/home.component.ts
+++ UI/fia/src/app/home/home.component.ts
@@ -41,4 +41,8 @@ export class HomeComponent implements OnInit {
       })
     }
   }
+
+  closeChat() {
+    this.openChat = false;
+  }
 }
