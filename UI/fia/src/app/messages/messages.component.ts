52381e40386eba69fb8f3dc1c456970bb03dfe4f
Róbert Nagy <roister@windowslive.com>
2023-04-24 18:55:58 +0200

    Feat: Add messages window with form

--- UI/fia/src/app/messages/messages.component.ts
+++ UI/fia/src/app/messages/messages.component.ts
@@ -1,23 +1,44 @@
 // messages.component.ts
-import { Component } from '@angular/core';
-
-interface Message {
-  sender: string;
-  text: string;
-  timestamp: Date;
-  avatar: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { Message } from '../../models/message';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
   messages: Message[] = [
-    {
-      sender: 'John', text: 'Hello!', timestamp: new Date(), avatar: '/assets/images/cavia_wizard1.png' },
+    { sender: 'John', text: 'Hello!', timestamp: new Date(), avatar: '/assets/images/cavia_wizard1.png' },
     { sender: 'Alice', text: 'Hi there!', timestamp: new Date(), avatar: '/assets/images/tortoise_barbarian1.png' },
     // Add more messages as needed
   ];
+
+  newMessage: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+    // Initialize component
+  }
+
+  sendMessage() {
+    if (this.newMessage.trim() !== '') {
+      // Add logic to send new message and update the messages array
+      // For example, you can push the new message to the messages array
+      const newMsg: Message = {
+        sender: 'You', // Update with the sender of the new message
+        text: this.newMessage,
+        timestamp: new Date(),
+        avatar: '/assets/images/your_avatar.png' // Update with the avatar of the sender
+      };
+      this.messages.push(newMsg);
+      this.newMessage = '';
+    }
+  }
 }
+
+
+
+
+
