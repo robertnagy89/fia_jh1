72b11360bcd875dccf9f02ec68f0ed06e007da50
Róbert Nagy <roister@windowslive.com>
2023-05-10 19:08:09 +0200

    Feat: Add properties to User entity

--- API/FIA/FIA.Api/Models/User.cs
+++ API/FIA/FIA.Api/Models/User.cs
@@ -1,11 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace FIA.Api.Models
+public class User
 {
-    public class User
-    {
-        [Required]
-        [StringLength(20, MinimumLength =3, ErrorMessage="Name must be between {2}, and {1} characters!")]
-        public string Name { get; set; }
-    }
+    [Required]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be between {2} and {1} characters.")]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string Email { get; set; }
+
+    [Required]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+    public string Password { get; set; }
 }
