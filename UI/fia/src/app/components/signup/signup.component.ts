6e2e15de92a0cf5d88d0b31962e3de33fb3f8421
Róbert Nagy <roister@windowslive.com>
2023-05-11 17:49:09 +0200

    Feat: Add signupcomponent validation forms //now front-end validators match back-end validators

--- UI/fia/src/app/components/signup/signup.component.ts
+++ UI/fia/src/app/components/signup/signup.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators, ReactiveFormsModule } from '@angular/forms';
 import { environment } from '../../../environments/environment';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-signup',
@@ -11,51 +12,76 @@ import { environment } from '../../../environments/environment';
 export class SignupComponent implements OnInit {
   signupForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient)
+  {
     this.signupForm = this.formBuilder.group({
-      username: ['', Validators.required],
+      username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
       useremail: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      userpassword: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required]
     }, { validator: this.passwordMatchValidator });
   }
+
+  ngOnInit(): void {
+    
+  }
+
   passwordMatchValidator: ValidatorFn = (control: AbstractControl) => {
-    const password = control.get('password');
+    const password = control.get('userpassword');
     const confirmPassword = control.get('confirmPassword');
 
     if (password && confirmPassword && password.value !== confirmPassword.value) {
       confirmPassword.setErrors({ passwordMismatch: true });
-
     }
     else {
       confirmPassword?.markAsTouched();
     }
     return null;
   }
+
+
   signup() {
-    // Perform any additional client-side validation if needed
+    if (this.signupForm.valid) {
+      // Logic for sign up
+      // Create a user object with the necessary properties
+      console.log(this.signupForm.value);
+      const user: User = {
+        id: '00000000-0000-0000-0000-000000000000',
+        name: this.signupForm.value.username,
+        email: this.signupForm.value.useremail,
+        password: this.signupForm.value.userpassword
+      };
 
-    // Create a user object with the necessary properties
-    const user = {
-      name: this.signupForm.value.name,
-      email: this.signupForm.value.email,
-      password: this.signupForm.value.password
-    };
+      console.log(user);
 
-    console.log(user);
+      // Make an HTTP POST request to the backend signup endpoint
+      this.http.post(`${environment.apiUrl}api/Signup`, user).subscribe(
+        (response) => {
+          // Handle successful signup
+          console.log('Signup successful:', response);
+        },
+        (error) => {
+          // Handle signup error
+          console.error('Signup error:', error);
+        }
+      );
 
-    // Make an HTTP POST request to the backend signup endpoint
-    this.http.post(`${environment.apiUrl}api/signup`, user).subscribe(
-      (response) => {
-        // Handle successful signup
-        console.log('Signup successful:', response);
-      },
-      (error) => {
-        // Handle signup error
-        console.error('Signup error:', error);
+    }
+    else {
+      this.validateAllFormGroups(this.signupForm);
+      // Logic for error
+    }
+  }
+
+  private validateAllFormGroups(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(group => {
+      const control = formGroup.get(group);
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormGroups(control);
       }
-    );
+
+    })
   }
 }
