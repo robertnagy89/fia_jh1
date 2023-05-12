e7cb564fc89c721d8c34431ba8ad59c6471f60ca
Róbert Nagy <roister@windowslive.com>
2023-05-13 01:31:21 +0200

    Feat: Add UserController

---
+++ API/FIA/FIA.Api/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using FIA.Api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace FIA.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly FiaDbContext _context;
+        public UserController(FiaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User user)
+        {
+            if (user == null) return BadRequest();
+
+            User existingUser = await _context.Users.FirstOrDefaultAsync(x => x.Name == user.Name && x.Password == user.Password);
+            if (existingUser == null) return NotFound(new { Message = "User not found, or wrong password." });
+            return Ok(new { Message = "Login successful!"});
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody]User user)
+        {
+            if(user == null) return BadRequest();
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok(new { Message = "User Registered!" });
+        }
+
+    }
+}
