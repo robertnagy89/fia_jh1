b393c46025182e942c9e036b03adfe3e9fb21d44
Róbert Nagy <roister@windowslive.com>
2023-05-16 22:17:12 +0200

    Feat: Add token interceptor error handling

--- UI/fia/src/app/interceptors/token.interceptor.ts
+++ UI/fia/src/app/interceptors/token.interceptor.ts
@@ -3,24 +3,34 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const myToken = this.auth.getToken();
 
     if (myToken) {
       request = request.clone({
-        setHeaders: { Authorization: `Bearer ${myToken}`}
+        setHeaders: { Authorization: `Bearer ${myToken}` }
       });
     }
-    return next.handle(request);
+    return next.handle(request).pipe(catchError((err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status == 401) {
+          alert("Token is expired, login again.");
+          this.router.navigate(['login']);
+        }
+      }
+      return throwError(() => new Error("The unexpected has happened!"));
+    }));
   }
 }
