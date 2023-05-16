5baa35c715d1e70ba678bdc85e79aa79468b069c
Róbert Nagy <roister@windowslive.com>
2023-05-16 21:45:42 +0200

    Feat: Add list registered users to dashboard

--- UI/fia/src/app/components/dashboard/dashboard.component.ts
+++ UI/fia/src/app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/user';
 import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  public users: any = [];
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.getUsers()
+      .subscribe(res => {
+        this.users = res;
+      })
+  }
 
   logOut() {
     this.auth.signOut();
