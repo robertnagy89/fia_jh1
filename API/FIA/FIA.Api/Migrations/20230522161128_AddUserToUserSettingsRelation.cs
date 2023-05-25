3dd10777168bf17c6d0b5e5874317de501c42265
Róbert Nagy <roister@windowslive.com>
2023-05-25 22:06:31 +0200

    Feat: Add base relations between user and usersettings entities

---
+++ API/FIA/FIA.Api/Migrations/20230522161128_AddUserToUserSettingsRelation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FIA.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserToUserSettingsRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserSettingsId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserSettingsId",
+                table: "Users",
+                column: "UserSettingsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_UserSettings_UserSettingsId",
+                table: "Users",
+                column: "UserSettingsId",
+                principalTable: "UserSettings",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_UserSettings_UserSettingsId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserSettingsId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserSettingsId",
+                table: "Users");
+        }
+    }
+}
