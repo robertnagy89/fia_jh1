afb68dc2d6ea33ac1830555e2188dd10ea3a7aa5
Róbert Nagy <roister@windowslive.com>
2023-06-21 21:52:39 +0200

    Feat: Add abstraction to color types

--- UI/fia/src/app/components/user-settings/user-settings.component.ts
+++ UI/fia/src/app/components/user-settings/user-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnChanges, OnInit, Renderer2 } from '@angular/core';
+import { UserSettings } from '../../../models/usersettings';
 import { UserSettingsService } from '../../services/user-settings.service';
 
 @Component({
@@ -7,12 +8,15 @@ import { UserSettingsService } from '../../services/user-settings.service';
   styleUrls: ['./user-settings.component.css']
 })
 export class UserSettingsComponent {
-  primaryColor: string = '#313A43';
-  secondaryColor: string = '#74818C';
-  accentColor: string = '#A9EEE6';
-  accentColorHover: string = '#19F9DF';
-  dangerColor: string = '#dc3545';
-  textColor: string = '#ffffff';
+  userSettings: UserSettings = {
+    id: '00000000-0000-0000-0000-000000000000',
+    primaryColor: '#313A43',
+    secondaryColor: '#74818C',
+    accentColor: '#A9EEE6',
+    accentColorHover: '#19F9DF',
+    dangerColor: '#dc3545',
+    textColor: '#ffffff'
+  }
 
   constructor(private settings: UserSettingsService) { };
 
@@ -28,29 +32,36 @@ export class UserSettingsComponent {
     //  --danger-color: #dc3545;
     //}
     if (name == 'default') {
-      this.primaryColor = '#313A43';
-      this.secondaryColor = '#74818C';
-      this.textColor = '#ffffff';
-      this.accentColor = '#A9EEE6';
-      this.dangerColor = '#dc3545';
-      this.accentColorHover = '#19F9DF';
+      this.userSettings = {
+        id: '00000000-0000-0000-0000-000000000000',
+        primaryColor: '#313A43',
+        secondaryColor: '#74818C',
+        accentColor: '#A9EEE6',
+        accentColorHover: '#19F9DF',
+        dangerColor: '#dc3545',
+        textColor: '#ffffff'
+      }
     }
     else if (name == 'calm') {
-      this.primaryColor = '#625772';
-      this.secondaryColor = '#fefaec';
-      this.textColor = '#342D3E';
-      this.accentColor = '#A9EEE6';
-      this.dangerColor = '#f38181';
-      this.accentColorHover = '#19F9DF';
+      this.userSettings = {
+        id: '00000000-0000-0000-0000-000000000000',
+        primaryColor: '#625772',
+        secondaryColor: '#fefaec',
+        accentColor: '#342D3E',
+        accentColorHover: '#19F9DF',
+        dangerColor: '#f38181',
+        textColor: '#070D0D'
+      }
     }
     this.updateChanges();
   }
 
   updateChanges = (): void => {
-    document.documentElement.style.setProperty('--primary-color', this.primaryColor);
-    document.documentElement.style.setProperty('--secondary-color', this.secondaryColor);
-    document.documentElement.style.setProperty('--accent-color', this.accentColor);
-    document.documentElement.style.setProperty('--danger-color', this.dangerColor);
-    document.documentElement.style.setProperty('--primary-color', this.primaryColor);
+    document.documentElement.style.setProperty('--primary-color', this.userSettings.primaryColor);
+    document.documentElement.style.setProperty('--secondary-color', this.userSettings.secondaryColor);
+    document.documentElement.style.setProperty('--accent-color', this.userSettings.accentColor);
+    document.documentElement.style.setProperty('--danger-color', this.userSettings.dangerColor);
+    document.documentElement.style.setProperty('--primary-color', this.userSettings.primaryColor);
+    this.settings.saveSettings(this.userSettings);
   }
 }
