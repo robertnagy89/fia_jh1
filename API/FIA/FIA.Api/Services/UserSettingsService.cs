9a3c9ad58a78e89b1656ab0bc7e2f072e3e425ce
Róbert Nagy <roister@windowslive.com>
2023-06-22 21:38:43 +0200

    Feat: Add UserSettingsService

---
+++ API/FIA/FIA.Api/Services/UserSettingsService.cs
@@ -0,0 +1,30 @@
+﻿using FIA.Api.Data;
+using FIA.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIA.Api.Services
+{
+    public class UserSettingsService
+    {
+        private readonly FiaDbContext _context;
+
+        public UserSettingsService(FiaDbContext context)
+        {
+            _context = context;
+        }
+
+        internal Task<UserSettings> GetUserSettings(string userId)
+        {
+            {
+                var userSettings = _context.Users
+                    .Where(u => u.Id.ToString() == userId)
+                    .Select(u => u.UserSettings)
+                    .FirstOrDefaultAsync();
+                return userSettings;
+            }
+        }
+    }
+}
