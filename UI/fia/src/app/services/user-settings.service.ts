5062c8067e500aea5e268c44425448141f1abde8
Róbert Nagy <roister@windowslive.com>
2023-06-22 21:39:25 +0200

    Feat: Add user-settings service

--- UI/fia/src/app/services/user-settings.service.ts
+++ UI/fia/src/app/services/user-settings.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { UserSettings } from '../../models/usersettings';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,12 @@ export class UserSettingsService {
 
   constructor(private http: HttpClient) { }
 
-  getUserSettings() {
-    return this.http.get<UserSettings>(environment.apiUrl + 'api/user/me');
+  getUserSettings(): Observable<UserSettings> {
+    return this.http.get<UserSettings>(environment.apiUrl + 'api/user/mysettings');
   }
 
-  saveSettings(settings: UserSettings) {
+  saveSettings(settings: UserSettings): Observable<UserSettings> {
     console.log(settings);
-    return this.http.put<UserSettings>('api/user/me', settings);
+    return this.http.put<UserSettings>(environment.apiUrl + 'api/user/mysettings', settings);
   }
 }
